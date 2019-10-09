0ee1a9be90a2d1fa1b979a133823b800ac04454a
Alex Ruiz <alexruiz95@aol.com>
2019-10-09 14:15:34 -0400

    Classic remove tiff. Need to just add this to the main code in matlab

---
+++ remove_tif.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+directory = sys.argv[1] # './scene21'
+if not os.path.isdir(directory):
+    print("error in directory")
+
+
+filelistA = os.listdir(directory)
+
+
+replacable = '.tif'
+
+for filename in filelistA:
+    new = filename.replace(replacable,'')
+    old = os.path.join(os.path.abspath(directory),filename)
+    os.rename(old,os.path.join(os.path.abspath(directory),new))
