1a733016f126c426cc6c55c1858eff7dbec6cbf8
Alex Ruiz <alexruiz95@aol.com>
2019-11-10 20:44:46 -0500

    writes to ori correctly

--- Generate_ORI_files.m
+++ Generate_ORI_files.m
@@ -24,21 +24,21 @@ for n = 1 : length(Cameras)
    [~, R, t] = getCameraMatrix(Camera);
    
    % Convert from camera matrix to orentation matrix
-   C = 1 * R\t;
+   C = -1 * R\t;
    Rc = inv(R)';
    % AR % This is where I made Chanages 
 %    euler_angle = rotm2eul(R');  % calculates the angles 
-%    euler_angle = RotationAngles(Rc)';
-   euler_angle = rotm2eul(Rc,'XYZ');
+%    euler_angle = RotationAngles(Rc)';euler_angle = rotm2eul(Rc,'XYZ');
+   euler_angle = rotm2eul(R','XYZ');
    file_name = ['test\cal\cam',num2str(n),'.tif.ori']; % FileName 
    dlmwrite(file_name,1000.*C','delimiter','\t','precision','%.3f') % X Y Z of cameras % first line of ORI
    dlmwrite(file_name,euler_angle,'delimiter','\t','-append','precision','%.4f') % ANGLES Second Line
    dlmwrite(file_name,' ','delimiter','\t','-append') % Add a space
-   dlmwrite(file_name,Rc,'delimiter','\t','-append','precision','%.4f') % Write the matrix
+   dlmwrite(file_name,R','delimiter','\t','-append','precision','%.4f') % Write the matrix
    dlmwrite(file_name,' ','delimiter','\t','-append') % Add a space 
    dlmwrite(file_name,[0.0000 0.0000],'-append','delimiter','\t','precision','%.3f') % Xp Yp
-   dlmwrite(file_name,0.1*1000,'-append','precision','%.3f') % BACK Focal distance should be in
-   dlmwrite(file_name,[0.001 0.001 0.001],'-append','delimiter','\t','precision','%.3f') % Last line
+   dlmwrite(file_name,40,'-append','precision','%.3f') % BACK Focal distance should be in
+   dlmwrite(file_name,[0.001 0.001 -1],'-append','delimiter','\t','precision','%.3f') % Last line
    
    
 %    ori_matrix{n} = Rc
