588b67049e2a7bd4a39c22c5e0e902ad4f1a040c
Alex Ruiz <alexruiz95@aol.com>
2019-11-25 17:42:43 -0500

    changed positions to be less sym

--- defaultCameraArrangement.m
+++ defaultCameraArrangement.m
@@ -2,16 +2,20 @@ function CAMERAS = defaultCameraArrangement()
    
     % Camera positions  % 
     % AR Adding these x_p y_p z_p I think the defaults where to high 
-    x_p = .12;  %METERS
-    y_p = .12;  %METERS
-    z_p = .8;   %METERS
-%     x_p = .2;  %METERS
-%     y_p = .2;  %METERS
+%     x_p = .12;  %METERS
+%     y_p = .12;  %METERS
 %     z_p = .8;   %METERS
+    x_p = .2;  %METERS
+    y_p = .2;  %METERS
+    z_p = .8;   %METERS
+    % Changed this up to have a little more random positions
+    tx = x_p * [-1, 1.01, -.95, .92];
+    ty = y_p * [1,  .99, -.99, -.85];
+    tz = z_p * [.98, .95, .92, 1];
     
-    tx = x_p * [-1, 1, -1, 1];
-    ty = y_p * [1,  1, -1, -1];
-    tz = z_p * [1, 1, 1, 1];
+%     tx = x_p * [-1, 1, -1, 1];
+%     ty = y_p * [1,  1, -1, -1];
+%     tz = z_p * [1, 1, 1, 1];
     
 %%% ---- DO NOT TOUCH BELOW ---- %%% 
     
