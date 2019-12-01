739c76c98c6cd0e10536354022134e36e0326226
Alex Ruiz <alexruiz95@aol.com>
2019-12-01 10:15:12 -0500

    Cleaned up comments and paths

--- Script_to_run.m
+++ Script_to_run.m
@@ -1,9 +1,20 @@
 
-% clear all
+% It is recommend to compile your own mex c function. To do this 
+% Delete:      generateParticleImage.mexw64
+
+% Then run compile_generateParticleImage() 
+% This will compile the C code
+% only need to do this once
+
+% add paths 
+addpath('plotting_camera');
+addpath('ptvcodes');
+addpath('test');
+
 
 % Generate the calibration Images     
 % 
-calTargetMultiView('save',true,'cal_dir','test2/cal','TargetFile', true,'target_3D',true,'generate_ori', true)
+calTargetMultiView('save',true,'cal_dir','test/cal','TargetFile', true,'target_3D',true,'generate_ori', true)
 % This code will save the images to the specified directory, create a
 % target file, generate the ori files for a 3D calibration plate
 
@@ -12,25 +23,35 @@ calTargetMultiView('save',true,'cal_dir','test2/cal','TargetFile', true,'target_
 % Generate_ORI_files()
 
 % Make the images
-makeImages2('outdir','test2/img','save',true,'plot',true);
+makeImages2('outdir','test/img','save',true,'plot',true);
 
 
 % Remove the tiff heading using python
-system('py remove_tif.py test2/img/Cam1');
-system('py remove_tif.py test2/img/Cam2');
-system('py remove_tif.py test2/img/Cam3');
-system('py remove_tif.py test2/img/Cam4');
+system('py remove_tif.py test/img/Cam1');
+system('py remove_tif.py test/img/Cam2');
+system('py remove_tif.py test/img/Cam3');
+system('py remove_tif.py test/img/Cam4');
 % or matlab
 %remove_tif_M.m
 
 % RUN PTV
 % Perform Calibration 
+% Note: In the calibration process you will notice that you will not be able to click “Fine tuning” after selecting “Raw orientation”. 
+% In order to overcome this bug you will need to perform these steps after selecting “ Raw Orientation” :
+% 1.	Click: Select Orient. With file
+% 2.	Click: Show initial guess
+% 3.	Click: Sortgrid
+% 4.	Click: Raw orientation % after this the finetuning button will activate
+% 5.	Click: Fine tuning 
+% It should work after this.
+
+
 % Perform Tracking 
 
 % Post Processsing 
 % In the PTVCODES run the code run_this.m to inspect the tracks
 
-
+% -------------- % 
 % PARAMS.g = 5;
 % PARAMS.a = 1;
 % PARAMS.m = 1e-6;
