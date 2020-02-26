c517fee3718eaf10f482631a1fe176dc60abbff7
Alex Ruiz <alexruiz95@aol.com>
2020-02-26 11:45:08 -0500

    generates grid for visualizing distortion

--- uniformgrid.m
+++ uniformgrid.m
@@ -1,4 +1,4 @@
-function [uniform_grid] = uniformgrid(min,max,n_lines)
+function [xx,yy,zz] = uniformgrid(min,max,n_lines,n_points)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -15,7 +15,7 @@ x = [min , max] ;
 y = [min , max] ;
 % Number of points to generate
 nx = n_lines ;
-ny = 100 ; 
+ny = n_points ; 
 x_vect = linspace(x(1),x(2),nx) ; 
 y_vect = linspace(y(1),y(2),ny) ;
 % Generate Mesh 
@@ -24,10 +24,14 @@ y_vect = linspace(y(1),y(2),ny) ;
 % Put it into an array 
 x_values = cat(1,X(:),Y(:));
 y_values = cat(1,Y(:),X(:));
-image_values = [x_values,y_values]
+z_values = zeros(size(x_values));
+image_values = [x_values,y_values,z_values]
 % Check to see if the output is correct 
 % plot(image_values(:,1),image_values(:,2),'.')
 uniform_grid = image_values ; 
 
+xx = x_values ;
+yy = y_values ;
+zz = z_values ;
 end
 
