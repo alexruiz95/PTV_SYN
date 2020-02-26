0344668087517b1bff76137baa3975a3718c6f69
Alex Ruiz <alexruiz95@aol.com>
2020-02-26 11:48:30 -0500

    na

--- defaultCameraArrangement.m
+++ defaultCameraArrangement.m
@@ -5,14 +5,16 @@ function CAMERAS = defaultCameraArrangement()
 %     x_p = .12;  %METERS
 %     y_p = .12;  %METERS
 %     z_p = .8;   %METERS
-    x_p = .2;  %METERS
-    y_p = .2;  %METERS
-    z_p = .8;   %METERS
-    % Changed this up to have a little more random positions
+    x_p = .2;  %METERS .2
+    y_p = .2;  %METERS .2
+    z_p = .95;   %METERS .8
+%     Changed this up to have a little more random positions
     tx = x_p * [-1, 1.01, -.95, .92];
     ty = y_p * [1,  .99, -.99, -.85];
     tz = z_p * [.98, .95, .92, 1];
-    
+%     tx = 0 * [-1, 1.01, -.95, .92];
+%     ty = 0 * [1,  .99, -.99, -.85];
+%     tz = 1 * [.98, .95, .92, 1];
 %     tx = x_p * [-1, 1, -1, 1];
 %     ty = y_p * [1,  1, -1, -1];
 %     tz = z_p * [1, 1, 1, 1];
