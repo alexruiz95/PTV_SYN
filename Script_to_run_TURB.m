5d8c9ca9d54c846ea65b1562b94e79b89832c39e
Alex Ruiz <alexruiz95@aol.com>
2020-03-30 13:51:17 -0400

    Rescaled, shifted (IT WORKS)

--- Script_to_run_TURB.m
+++ Script_to_run_TURB.m
@@ -26,7 +26,7 @@ calTargetMultiView('save',true,'cal_dir',cal_dir,'TargetFile', true,'target_3D',
 
 
 % Generate the ORI Files for initial guess (exact) Manually with 
-%Generate_ORI_files('ori_dir',cal_dir)
+Generate_ORI_files('ori_dir',cal_dir)
 % NOTE: CalTartMultiView Takes care of this
 
 % Make the images
@@ -75,10 +75,10 @@ remove_tif_M(img_dir)
 
 test=['C:\Users\alex\Desktop\og\PTV_SYN\',work_dir,'\res'];
 % test='C:\Users\alex\Desktop\og\PTV_SYN\test\Copy_of_res';
-start = 10020;
-last = 10090;
+start = 10001;
+last = 10075;
 dt=1/100;
-min_len = 2;
+min_len = 5;
 %------
 
 % main = 'C:\Users\alex\OneDrive - Knights - University of Central Florida\Documents\research_xray\CODE_MAIN\TEST DATA FOR PTV\';
@@ -87,32 +87,51 @@ min_len = 2;
 traj_RC=ptv_is_to_traj(test,start,last,min_len,dt);
 plot_long_trajectories(traj_RC,min_len);
 title('Turbulent Flow- Reconstruction')
-saveas(gcf,'turb_flow_reconstruct.png')
+% saveas(gcf,'turb_flow_reconstruct.png')
 
 % Ground TRUTH DATA
 % Load the data 
+% clear all
+% close all
 load('test.mat')
 N_TRAJ = 100 ;
+dt=1/100;
+min_len = 2;
 for time = 1:length(t)
         X(time,:) = x(time,1:N_TRAJ,1);
         Y(time,:) = x(time,1:N_TRAJ,2);
         Z(time,:) = x(time,1:N_TRAJ,3);
 end
 
-% scale = 20;
-% X=(X-((max(max(X))+min(min(X)))/2))/scale ;
-% Y=(Y-((max(max(Y))+min(min(Y)))/2))/scale ;
-% Z=(Z-((max(max(Z))+min(min(Z)))/2))/scale ;
-
-X = X*10;
-Y = Y*10;
-Z = Z*10;
-
-traj_GT = pos_to_traj(X,Y,Z,dt)
-plot_long_trajectories(traj_GT,min_len)
+% FIRST MULT BY .01 to scale from [cm] to [m]
+X=.01*X;
+Y=.01*Y;
+Z=.01*Z;
+% SHIFT POINTS 
+X1 = (X - mean(mean(X)));
+Y1 = (Y - mean(mean(Y)));
+Z1 = (Z - min(min(Z)));
+% PLOT
+plot3(X1,Y1,Z1,'.')
+xlabel('x');ylabel('y');zlabel('z')
+traj_GT = pos_to_traj(X1,Y1,Z1,dt);
+% traj_GT_mm = pos_to_traj(X1./1000,Y1./1000,Z1./1000,dt)
+plot_long(traj_GT,min_len)
+% plot_long_trajectories(traj_GT,min_len)
+set(gca,'xdir','reverse')
+set(gca, 'ydir', 'reverse')
+set(gca, 'zdir', 'normal')
 title('Turbulent Flow-Ground Truth')
-saveas(gcf,'turb_flow_GT.png')
+% saveas(gcf,'turb_flow_GT.png')
 
+
+% COMPARE THE TWO PLOTS 
+plot_long_traject_COP(traj_GT,'Ground Truth [m]',traj_RC,'Reconstruction [mm]',min_len)
+
+%------------------------------------------------
+
+% [Image] = makeImages3;
+% implay(Image(:,:,:,:,1))
 
 %-dep-%
 % -------------- % 
@@ -120,3 +139,7 @@ saveas(gcf,'turb_flow_GT.png')
 % PARAMS.a = 1;
 % PARAMS.m = 1e-6;
 % makeImages2('outdir','test2/img','save',true,'plot',true,'velocityFunctionParams',PARAMS,'velocityFunction', @burgersVortex,'zrange', [-.1,.1],'particleDiameterMean', 1.5*sqrt(8),'particleConcentration', 1.5e4)
+
+%
+%text_2_disp = ['X-Range:',num2str([min(X1(:)),max(X1(:))]),'; Y-Range:',num2str([min(Y1(:)),max(Y1(:))]),'; Z-Range:',num2str([min(Z1(:)),max(Z1(:))])];
+%disp(text_2_disp)
