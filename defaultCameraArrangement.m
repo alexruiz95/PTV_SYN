1ff386a54f29478f52f550d0768b253aca9a9447
Alex Ruiz <alexruiz95@aol.com>
2020-04-05 19:07:15 -0400

    moved cameras closer

--- defaultCameraArrangement.m
+++ defaultCameraArrangement.m
@@ -2,13 +2,17 @@ function CAMERAS = defaultCameraArrangement()
    
 %----  Camera positions  ----% 
 
-    x_p = .2;  %METERS .2    % .2
-    y_p = .2;  %METERS .2    % .2
-    z_p = .95;   %METERS .8  % .95
+    x_p = .05;  %METERS .2    % .2
+    y_p = .05;  %METERS .2    % .2
+    z_p = .2;   %METERS .8  % .95
 %  Changed this up to have a little more random positions
-    tx = x_p * [-1, 1.01, -.95, .92];
-    ty = y_p * [1,  .99, -.99, -.85];
-    tz = z_p * [.98, .95, .92, 1];
+%     tx = x_p * [-1, 1.01, -.95, .92];
+%     ty = y_p * [1,  .99, -.99, -.85];
+%     tz = z_p * [.98, .95, .92, 1];
+
+    tx = x_p * [-1, 1, -1, 1];
+    ty = y_p * [1,  1, -1, -1];
+    tz = z_p * [1, 1, 1, 1];
     
 %%% ---- DO NOT TOUCH BELOW ---- %%% 
     
