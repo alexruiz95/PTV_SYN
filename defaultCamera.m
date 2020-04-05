93c291aa8dcda3e763f09a8ab75dbccaf4ec49f2
Alex Ruiz <alexruiz95@aol.com>
2020-04-05 19:07:31 -0400

    changed focal distance to 200 was 105

--- defaultCamera.m
+++ defaultCamera.m
@@ -5,14 +5,14 @@ function CAMERA = defaultCamera()
     CAMERA = struct();
         
     % Populate  Camera parameters
-    CAMERA.FocalLength      = 0.105;   % Meters
+    CAMERA.FocalLength      = 0.205;   % Meters Usually .105
     CAMERA.PixelHeight      = 1.7E-5;  % Meters 0.0065 mm
     CAMERA.PixelWidth       = 1.7E-5;  % Meters
     CAMERA.PixelRows        = 1024;    % Pixels
     CAMERA.PixelColumns     = 1024;    % Pixels
-    CAMERA.SensorNoiseStd   = 0.05;    % Fraction of saturation intensity
-    CAMERA.SensorNoiseMean  = 0.15;    % Fraction of saturation intensity
-    CAMERA.SensorGain       = 0.6;     % Unitless
+    CAMERA.SensorNoiseStd   = 0.05;    % Fraction of saturation intensity 0.05
+    CAMERA.SensorNoiseMean  = 0.05;    % Fraction of saturation intensity 0.15
+    CAMERA.SensorGain       = 0.6;     % Unitless 0.6
  %--% DO NOT TOUCH THESE UNLESS YOU KNOW WHAT YOU ARE DOING %--%
     CAMERA.Eye              = [0,0,1]; % Meters
     CAMERA.Center           = [0,0,0]; % Meters
